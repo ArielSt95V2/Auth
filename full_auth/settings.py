a2bdfce050d55770c50823d1c4ed493926a0754d
ArielSt95V2 <astablediffusion2@gmail.com>
2025-12-13 19:07:40 +0200

    fix attempt 1 log

--- full_auth/settings.py
+++ full_auth/settings.py
@@ -224,3 +224,18 @@ CORS_ALLOW_CREDENTIALS = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.UserAccount'
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
